daecfdadf8e792420a478b0bd768128ea7b8299d
Humendra <humendrx@gmail.com>
2022-11-08 14:53:30 +0530

    done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,36 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A first meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Staten_Island_Ferry_%28New_York%29.jpg/1920px-Staten_Island_Ferry_%28New_York%29.jpg',
+    address: 'New York',
+    description: 'A great meetup ever',
+  },
+  {
+    id: 'm2',
+    title: 'A second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Staten_Island_Ferry_%28New_York%29.jpg/1920px-Staten_Island_Ferry_%28New_York%29.jpg',
+    address: 'New York',
+    description: 'A huge meetup ever',
+  },
+  {
+    id: 'm3',
+    title: 'A third meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Staten_Island_Ferry_%28New_York%29.jpg/1920px-Staten_Island_Ferry_%28New_York%29.jpg',
+    address: 'New York',
+    description: 'A small meetup ever',
+  },
+];
+
 const HomePage = () => {
-  return <h1>HomePage</h1>;
+  return (
+      <MeetupList meetups={DUMMY_MEETUPS} />
+  )
 };
 
 export default HomePage;
